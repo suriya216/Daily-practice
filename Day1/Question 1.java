10f047c0dd3242a54153d1c646662bf9f47bf36b
Suriya216 <sssuriya003@gmail.com>
2024-03-27 20:40:11 +0530

    Question 1 answer update

--- Day1/Question 1.java
+++ Day1/Question 1.java
@@ -1,1 +1,25 @@
 package Day1;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+class Question1{
+    public static int helper(String[] str){
+        Stack<String> stack = new Stack<>();
+        for (String word : str) {
+            if (!stack.isEmpty() && stack.peek().equals(word)) {
+                stack.pop(); 
+            } else {
+                stack.push(word); 
+            }
+        }
+        
+        return stack.size();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(helper(str.split(" ")));
+        sc.close();
+    }
+}
